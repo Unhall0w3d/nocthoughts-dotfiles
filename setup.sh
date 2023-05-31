7b24987a65f10d372d711e0171be55a024e0e137
Unhall0w3d <kenoperry@gmail.com>
2023-05-30 20:55:45 -0400

    Ready for testing -- works successfully on ArcoLinuxB - Hyprland/Hypr installs. Checking Arch Minimal (target).

--- setup.sh
+++ setup.sh
@@ -41,15 +41,22 @@ if [[ $install_packages == "Y" || $install_packages == "y" ]]; then
     sudo bash -c 'echo -e "[arcolinux_repo]\nSigLevel = Optional TrustedOnly\nInclude = /etc/pacman.d/arcolinux-mirrorlist\n" >> /etc/pacman.conf'
     sudo bash -c 'echo -e "[arcolinux_repo_3party]\nSigLevel = Optional TrustedOnly\nInclude = /etc/pacman.d/arcolinux-mirrorlist\n" >> /etc/pacman.conf'
     sudo bash -c 'echo -e "[arcolinux_repo_xlarge]\nSigLevel = Optional TrustedOnly\nInclude = /etc/pacman.d/arcolinux-mirrorlist\n" >> /etc/pacman.conf'
+    # Move mirrorlist files to their proper location
     echo -e "$CNT - Placing mirrorlists... "
     sudo cp ./etc/pacman.d/arcolinux-mirrorlist /etc/pacman.d/
+
+    # Update Paru databases
     echo -e "$CNT - Updating Paru databases... " 
-    paru -Syu --noconfirm
+    paru -Sy --noconfirm
+
+    # Install Waybar
     echo -e "$CNT - Installing Waybar... " 
     paru -S --noconfirm gcc12
     export CC=gcc-12 CXX=g++-12
     paru -S --noconfirm waybar-hyprland-git 
     echo -e "$CNT - Waybar installed."
+
+    # Installing Core Components
     echo -e "$CNT - Installing main components... " 
     for pkg in hyprland-dev wezterm swaylock-effects swaybg wofi wlogout rofi rofi-emoji mako xdg-desktop-portal-hyprland swappy grimblast-git slurp thunar xorg-xhost python python-pyxdg xorg-bdftopcf xorg-fonts-encodings xorg-iceauth xorg-mkfontscale xorg-server xorg-server-common xorg-sessreg xorg-setxkbmap xorg-smproxy xorg-x11perf xorg-xauth xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xkbevd xorg-xkbprint xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xmodmap xorg-xpr xorg-xprop xorg-xrandr xorg-xrdb xorg-refresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwayland xorg-xwd xorg-xwininfo xorg-xwud xorgproto; do
         if ! pacman -Qs "$pkg" > /dev/null ; then
@@ -59,6 +66,8 @@ if [[ $install_packages == "Y" || $install_packages == "y" ]]; then
             echo -e "$CNT - $pkg is already installed" 
         fi
     done
+    
+    # Installing Utilities
     echo -e "$CNT - Installing utilities... " 
     for pkg in mpv polkit-gnome pavucontrol brightnessctl bluez bluez-utils blueman network-manager-applet gvfs thunar-archive-plugin file-roller neofetch neovim ranger fnm noise-suppression-for-voice viewnior cava ripgrep ffmpegthumbnailer btop dunst wl-clipboard wf-recorder hyprpicker-git hyprpaper-git tumbler imagemagick ncspot pix; do
         if ! pacman -Qs "$pkg" > /dev/null ; then
@@ -68,6 +77,8 @@ if [[ $install_packages == "Y" || $install_packages == "y" ]]; then
             echo -e "$CNT - $pkg is already installed" 
         fi
     done
+
+    # Installing ZSH and related plugins
     echo -e "$CNT - Installing zsh, plugins... " 
     for pkg in zsh oh-my-zsh-git zsh-autosuggestions zsh-autocomplete-git zsh-autoswitch-virtualenv-git zsh-doc zsh-history-substring-search; do
         if ! pacman -Qs "$pkg" > /dev/null ; then
@@ -77,8 +88,10 @@ if [[ $install_packages == "Y" || $install_packages == "y" ]]; then
             echo -e "$CNT - $pkg is already installed" 
         fi
     done
+
+    # Installing theme utilities, themes, icons, fonts
     echo -e "$CNT - Installing themes, theme utilities... " 
-    for pkg in arcolinux-candy-beauty-git wtype colord qt5-wayland qt6-wayland sweet-cursors-theme-git sweet-folders-icons-git sweet-gtk-theme-dark sweet-kvantum-theme kvantum nwg-look xfce4-settings ttf-cascadia-code-nerd ttf-cascadia-code-git sddm-git arcolinux-sddm-sugar-candy-git lxappearance; do
+    for pkg in arcolinux-candy-beauty-git wtype colord qt5-wayland qt6-wayland sweet-cursors-theme-git sweet-folders-icons-git sweet-gtk-theme-dark sweet-kvantum-theme-git kvantum nwg-look xfce4-settings ttf-cascadia-code-nerd ttf-cascadia-code-git sddm-git arcolinux-sddm-sugar-candy-git lxappearance; do
         if ! pacman -Qs "$pkg" > /dev/null ; then
             echo -e "$CNT - Installing $pkg... " 
             paru -Sy --noconfirm "$pkg"
@@ -115,6 +128,20 @@ if [[ $copy_files == "Y" || $copy_files == "y" ]]; then
     echo -e "$CNT - Config files have been moved." 
 fi
 
+# Set Scripts Executable
+echo -e "$CNT - Setting scripts to executable... " 
+sudo chmod +x ~/.scripts/colorpicker 
+sudo chmod +x ~/.scripts/IOMMU.sh 
+sudo chmod +x ~/.scripts/screensht
+sudo chmod +x ~/.scripts/xhost.sh
+sudo chmod +x ~/.scripts/notifications
+sudo chmod +x ~/.scripts/startup
+sudo chmod +x ~/.scripts/wofi-beats
+sudo chmod +x ~/.scripts/xauthority
+sudo chmod +x ~/.scripts/statusbar
+sudo chmod +x ~/.scripts/keyhint
+sudo chmod +x ~/.scripts/gtkthemes
+
 # Copy the powerlevel10k theme over
 echo -e "$CNT - Copying powerlevel10k theme over... " 
 sudo cp -r "usr" "/" 
@@ -131,6 +158,12 @@ if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
     echo -e "$COK - NetworkManager restarted successfully." 
 fi
 
+# Setup some theme settings
+echo -e "$CNT - Setting up SDDM Background... "
+sudo cp -r sddm_theme/ /usr/share/sddm/themes
+sudo rm /usr/share/sddm/themes/sddm_theme/wallpaper.jpg
+sudo cp ./sddm_theme/Backgrounds/wallpaper-dark.jpg /usr/share/sddm/themes/sddm_theme/wallpaper.jpg
+
 # Enable Services
 echo -e "$CNT - Starting Bluetooth Services... " 
 sudo systemctl enable --now bluetooth.service 
@@ -145,18 +178,8 @@ paru -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-desktop-
 
 # Reset Font Cache
 echo -e "$CNT - Rebuilding font cache... " 
-sudo fc-cache -f -v 
+fc-cache -f -v 
 echo -e "$COK - Font cache rebuilt." 
-
-# Set Scripts Executable
-echo -e "$CNT - Setting scripts to executable... " 
-sudo chmod +x ~/.scripts/colorpicker 
-sudo chmod +x ~/.scripts/IOMMU.sh 
-sudo chmod +x ~/.scripts/screensht
-sudo chmod +x ~/.scripts/xhost.sh
-
-# Setup some theme settings
-sudo cp /usr/share/sddm/themes/sddm_theme/Backgrounds/wallpaper-dark.jpg /usr/share/sddm/themes/sddm_theme/wallpaper.jpg
 
 # Script Complete
 echo -e "$COK - Script Completed!" 
